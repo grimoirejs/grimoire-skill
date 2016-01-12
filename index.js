756046b25e6dd6d5d74fdcb3a6820e5613438532
Matt Buck <techpeace@gmail.com>
2016-01-12 04:10:53 -0600

    Add initial Skill class with test

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * grimoire-skill
+ *
+ * @license MIT
+ * @copyright Voxable 2016
+ */
+
+'use strict';
+
+module.exports = require( './lib/grimoire-skill' );
---
+++ test/grimoire-skill.js
@@ -0,0 +1,24 @@
+/**
+ * grimoire-skill
+ *
+ * @license MIT
+ * @copyright Voxable 2016
+ */
+
+'use strict';
+
+/*eslint-env node, mocha*/
+
+let expect = require( 'chai' ).expect;
+let Skill = require( '../' );
+
+describe( 'Skill', function() {
+  describe( 'constructor', function() {
+    it( 'sets the name to the value of the first argument', function() {
+      let skillName = 'sample';
+      let subject = new Skill( skillName );
+
+      expect( subject.name ).to.equal( skillName );
+    } );
+  } );
+} );
